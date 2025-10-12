d3d42d6c0b6a0d12e8c8486cf567492cc2fdcb24
andyvvuong <166438748+andyvvuong@users.noreply.github.com>
2025-10-11 19:29:23 -0700

    adding domain1_mod that works for part 3 w/ pipeline delay

--- Stream_Interleaver.srcs/sources_1/new/domain1.v
+++ Stream_Interleaver.srcs/sources_1/new/domain1.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 10/09/2025 10:44:22 PM
+// Create Date: 10/10/2025 10:41:20 PM
 // Design Name: 
-// Module Name: domain1
+// Module Name: domain1_mod
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module domain1(
+module domain1_mod(
     input clk, rst,sel,
     input [7:0] inp1, //P0
     input [7:0] inp2, //P1
@@ -30,6 +30,12 @@ module domain1(
     output reg [7:0] A_value //Either has Alpha or one minus alpha
     );
     
+    wire [7:0] P0_delay;
+    c_shift_ram_0 shift_gen(.CLK(clk),
+                          .D(inp1),
+                          .Q(P0_delay)
+                          );
+    
     reg [7:0] P0_reg, P1_reg, A_reg, A_minus_reg;
     always @(posedge clk or negedge rst) begin
         if(!rst) begin
@@ -38,7 +44,7 @@ module domain1(
             A_reg <= 8'b0;
             A_minus_reg <= 8'b0;
         end else begin
-            P0_reg <= inp1;
+            P0_reg <= P0_delay;
             P1_reg <= inp2;
             A_reg <= inp3;
             A_minus_reg <= inp4;
@@ -49,6 +55,5 @@ module domain1(
         P_value = sel ? P0_reg : P1_reg; //T = P0_reg, F = P1_reg
         A_value = sel ? A_reg : A_minus_reg; //T = A_reg, F = A_minus_reg
     end
-    
     
 endmodule
